7352c8ee64415c8ff85988802c6aed21ef2e513f
Ranjith <ranjith13119@gmail.com>
2022-03-15 08:06:37 +0000

    Credentials store access

---
+++ app/resources/fiori/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function (AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("BTP_AP_CREDENTIALS_STORE-fiori.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
